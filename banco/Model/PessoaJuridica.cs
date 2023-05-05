70be8ed3be6f8885703c5b8f4bb6adf4a0eeee65
Beatriz Ceschini <biaceschini@gmail.com>
2023-05-05 20:56:44 -0300

    Classe PessoaJuridica

---
+++ banco/Model/PessoaJuridica.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using banco.Model;
+
+namespace banco.Model
+{
+  public class PessoaJuridica : Cliente
+  {
+    public string CNPJ { get; set; }
+    public string RazaoSocial { get; set; }
+    public string InscricaoEstadual { get; set; }
+
+    public PessoaJuridica()
+    {
+
+    }
+
+    public PessoaJuridica(decimal limiteMaximo, string numeroConta, string nome, string endereco, string email, string telefone,
+                        string cnpj, string razaoSocial, string inscricaoEstadual)
+                        : base(limiteMaximo, numeroConta, nome, endereco, email, telefone)
+    {
+      CNPJ = cnpj;
+      RazaoSocial = razaoSocial;
+      InscricaoEstadual = inscricaoEstadual;
+    }
+
+  }
+}
