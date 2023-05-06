b2c187c0f92f597db0a81c4925fef1a80051404e
Beatriz Ceschini <biaceschini@gmail.com>
2023-05-05 22:27:05 -0300

    Menu de aplicação para o projeto do banco

--- banco/Program.cs
+++ banco/Program.cs
@@ -5,9 +5,50 @@ internal class Program
 {
   private static void Main(string[] args)
   {
-    ClienteService clienteService = new ClienteService();
+    ClienteService _clienteService = new ClienteService();
+    string opcao = "";
 
-    clienteService.CriarConta();
-    clienteService.ExibirClientes();
+    do
+    {
+      Console.WriteLine("........................................................................\n");
+      Console.WriteLine("                       Bem vindo ao banco c#");
+      Console.WriteLine("........................................................................\n");
+      Console.WriteLine("________________________________________________________________________");
+      Console.WriteLine("                    O que você deseja realizar hoje?");
+      Console.WriteLine("                    * Digite : 1 - Abrir Conta");
+      Console.WriteLine("                    * Digite : 2 - Consultar Conta");
+      Console.WriteLine("                    * Digite : 3 - Listar Todas as contas");
+      Console.WriteLine("                    * Digite : 4 - SAIR");
+      Console.WriteLine("________________________________________________________________________\n");
+      Console.Write("\n                       Digite uma opção :  ");
+      opcao = Console.ReadLine();
+      System.Console.WriteLine("\n");
+      switch (opcao)
+      {
+        case "1":
+          _clienteService.CriarConta();
+          break;
+        case "2":
+          Console.Write("Digite o numero da conta:");
+          var cliente = _clienteService.BuscarClientePorNumeroDeConta(Console.ReadLine());
+          if (cliente == null)
+          {
+            Console.WriteLine("Conta não Cadastrada");
+          }
+          else
+          {
+            Console.WriteLine(cliente.ResumoCliente());
+          }
+          break;
+        case "3":
+          _clienteService.ExibirClientes();
+          break;
+        case "4":
+          break;
+        default:
+          Console.WriteLine("Opção não encontrada. Tente novamente! \n\n\n");
+          break;
+      }
+    } while (opcao != "4");
   }
 }
